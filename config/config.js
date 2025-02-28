c789fee03165d764e16ecd54c5891b7b9912cca7
JesusBTE <jeisbetancourtes@ittepic.edu.mx>
2025-02-27 21:29:39 -0700

    Agregando archivos

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 
 // Importamos el archivo de configuración con las credenciales de Firebase
-const serviceAccount = require("../config/tasks-4afe2-firebase-adminsdk-fbsvc-6c5be679e8.json");
+const serviceAccount = require("../config/key.json");
 
 // Inicializamos la aplicación de Firebase con las credenciales de administrador
 admin.initializeApp({
